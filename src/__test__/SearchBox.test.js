68c6c76b31aab92e5b2705c1b0691029dcf50599
Rob <roberto.reale.ja@gmail.com>
2021-01-09 16:41:24 +0900

    Add test for custom items structure

--- src/__test__/SearchBox.test.js
+++ src/__test__/SearchBox.test.js
@@ -4,24 +4,7 @@ import { fireEvent, cleanup, render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import ReactSearchAutocomplete from '../ReactSearchAutocomplete'
 
-const items = [
-  {
-    id: 0,
-    name: 'value0'
-  },
-  {
-    id: 1,
-    name: 'value1'
-  },
-  {
-    id: 2,
-    name: 'value2'
-  },
-  {
-    id: 3,
-    name: 'value3'
-  }
-]
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 beforeEach(() => {
   localStorage.clear()
@@ -32,15 +15,32 @@ afterEach(() => {
   jest.clearAllMocks()
 })
 
-const defaultProps = {
-  items: items,
-  placeholder: 'Search',
-  onSearch: () => {}
-}
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
 describe('<ReactSearchAutocomplete>', () => {
+  let items = [
+    {
+      id: 0,
+      name: 'value0'
+    },
+    {
+      id: 1,
+      name: 'value1'
+    },
+    {
+      id: 2,
+      name: 'value2'
+    },
+    {
+      id: 3,
+      name: 'value3'
+    }
+  ]
+
+  let defaultProps = {
+    items: items,
+    placeholder: 'Search',
+    onSearch: () => {}
+  }
+
   test('Renders the search box', () => {
     const { queryByPlaceholderText, container } = render(
       <ReactSearchAutocomplete {...defaultProps} />
@@ -52,38 +52,6 @@ describe('<ReactSearchAutocomplete>', () => {
     expect(container.getElementsByTagName('svg').length).toBe(1)
     // check that wrapper div is present
     expect(container.getElementsByClassName('wrapper').length).toBe(1)
-  })
-
-  test('Shows 4 matching items', () => {
-    const { queryByPlaceholderText, container } = render(
-      <ReactSearchAutocomplete {...defaultProps} />
-    )
-    const inputElement = queryByPlaceholderText(/search/i)
-    fireEvent.change(inputElement, { target: { value: 'v' } })
-    const ul = container.getElementsByTagName('ul')[0]
-    expect(ul.getElementsByTagName('li').length).toBe(4)
-    expect(ul.getElementsByTagName('svg').length).toBe(4)
-  })
-
-  test('Shows 1 matching item', () => {
-    const { queryByPlaceholderText, queryAllByTitle, container, debug } = render(
-      <ReactSearchAutocomplete {...defaultProps} />
-    )
-    const inputElement = queryByPlaceholderText(/search/i)
-    fireEvent.change(inputElement, { target: { value: '0' } })
-    expect(queryAllByTitle('value0').length).toBe(1)
-    const ul = container.getElementsByTagName('ul')[0]
-    expect(ul.getElementsByTagName('li').length).toBe(1)
-    expect(ul.getElementsByTagName('svg').length).toBe(1)
-  })
-
-  test('Shows 0 matching item', () => {
-    const { queryByPlaceholderText, container } = render(
-      <ReactSearchAutocomplete {...defaultProps} />
-    )
-    const inputElement = queryByPlaceholderText(/search/i)
-    fireEvent.change(inputElement, { target: { value: 'despair' } })
-    expect(container.getElementsByTagName('ul').length).toBe(0)
   })
 
   test("Don't use sessionStorage if useCaching is false", () => {
@@ -188,5 +156,117 @@ describe('<ReactSearchAutocomplete>', () => {
     }
     jest.runAllTimers()
     expect(onSearch).toBeCalledTimes(10)
+  })
+
+  describe('with items with name property', () => {
+    test('Renders the search box', () => {
+      const { queryByPlaceholderText, container } = render(
+        <ReactSearchAutocomplete {...defaultProps} />
+      )
+      const inputElement = queryByPlaceholderText(/search/i)
+      // check that the input node is present
+      expect(inputElement).toBeInTheDocument()
+      // check that the icon is present
+      expect(container.getElementsByTagName('svg').length).toBe(1)
+      // check that wrapper div is present
+      expect(container.getElementsByClassName('wrapper').length).toBe(1)
+    })
+
+    test('Shows 4 matching items', () => {
+      const { queryByPlaceholderText, container } = render(
+        <ReactSearchAutocomplete {...defaultProps} />
+      )
+      const inputElement = queryByPlaceholderText(/search/i)
+      fireEvent.change(inputElement, { target: { value: 'v' } })
+      const ul = container.getElementsByTagName('ul')[0]
+      expect(ul.getElementsByTagName('li').length).toBe(4)
+      expect(ul.getElementsByTagName('svg').length).toBe(4)
+    })
+
+    test('Shows 1 matching item', () => {
+      const { queryByPlaceholderText, queryAllByTitle, container, debug } = render(
+        <ReactSearchAutocomplete {...defaultProps} />
+      )
+      const inputElement = queryByPlaceholderText(/search/i)
+      fireEvent.change(inputElement, { target: { value: '0' } })
+      expect(queryAllByTitle('value0').length).toBe(1)
+      const ul = container.getElementsByTagName('ul')[0]
+      expect(ul.getElementsByTagName('li').length).toBe(1)
+      expect(ul.getElementsByTagName('svg').length).toBe(1)
+    })
+
+    test('Shows 0 matching item', () => {
+      const { queryByPlaceholderText, container } = render(
+        <ReactSearchAutocomplete {...defaultProps} />
+      )
+      const inputElement = queryByPlaceholderText(/search/i)
+      fireEvent.change(inputElement, { target: { value: 'despair' } })
+      expect(container.getElementsByTagName('ul').length).toBe(0)
+    })
+  })
+
+  describe('with items with custom properties property', () => {
+    const items = [
+      {
+        id: 0,
+        title: 'Titanic',
+        description: 'A movie about love'
+      },
+      {
+        id: 1,
+        title: 'Dead Poets Society',
+        description: 'A movie about poetry and the meaning of life'
+      },
+      {
+        id: 2,
+        title: 'Terminator 2',
+        description: 'A robot from the future is sent back in time'
+      },
+      {
+        id: 3,
+        title: 'Alien 2',
+        description: 'Ripley is back for a new adventure'
+      }
+    ]
+
+    const defaultProps = {
+      items: items,
+      placeholder: 'Search',
+      onSearch: () => {},
+      fuseOptions: { keys: ['title', 'description'] },
+      resultStringKeyName: 'title'
+    }
+
+    test('Shows 4 matching items', () => {
+      const { queryByPlaceholderText, container } = render(
+        <ReactSearchAutocomplete {...defaultProps} />
+      )
+      const inputElement = queryByPlaceholderText(/search/i)
+      fireEvent.change(inputElement, { target: { value: 'a' } })
+      const ul = container.getElementsByTagName('ul')[0]
+      expect(ul.getElementsByTagName('li').length).toBe(4)
+      expect(ul.getElementsByTagName('svg').length).toBe(4)
+    })
+
+    test('Shows 1 matching item', () => {
+      const { queryByPlaceholderText, queryAllByTitle, container, debug } = render(
+        <ReactSearchAutocomplete {...defaultProps} />
+      )
+      const inputElement = queryByPlaceholderText(/search/i)
+      fireEvent.change(inputElement, { target: { value: 'dead' } })
+      expect(queryAllByTitle('Dead Poets Society').length).toBe(1)
+      const ul = container.getElementsByTagName('ul')[0]
+      expect(ul.getElementsByTagName('li').length).toBe(1)
+      expect(ul.getElementsByTagName('svg').length).toBe(1)
+    })
+
+    test('Shows 0 matching item', () => {
+      const { queryByPlaceholderText, container } = render(
+        <ReactSearchAutocomplete {...defaultProps} />
+      )
+      const inputElement = queryByPlaceholderText(/search/i)
+      fireEvent.change(inputElement, { target: { value: 'despaira' } })
+      expect(container.getElementsByTagName('ul').length).toBe(0)
+    })
   })
 })
